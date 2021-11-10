fdf5ecff652c75c3b24e45d2512f244edbe1837f
Tony Collins <tjc@eltham-college.org.uk>
2021-11-10 14:36:08 +0000

    Add project files.

---
+++ Year11Functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Year11Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void YearGroup(int age)
+        {
+            // Complete this subroutine so that it prints a students year group
+            // to the console based on their age
+            // 11 - you are in year 7
+            // 12 - you are in year 8
+            // and so on 
+            // if age < 11 print or > 17 print out an error message
+
+        }
+
+        static int addThreeInts(int a, int b, int c)
+        {
+            int result=0;
+
+            return result;
+
+        }
+        // Challenging question
+        // write a function that takes the filename of a text file and a students first and second name
+        // and creates another file with the students name and initials e.g TB redacted(*'ed out)
+
+    }
+}
